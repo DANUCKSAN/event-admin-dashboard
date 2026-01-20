ec1fa55357457f720ab29f426c8d3c69693ce06a
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-01-20 14:14:15 +1100

    header-done-with-dummy-data

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,37 +1,62 @@
 import React from 'react'
+import { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import { deepOrange } from '@mui/material/colors';
+import { NavLink } from 'react-router';
+
+const user={
+  name: "Dan",
+  AvatarLetter: "D"
+}
 
 const Header = () => {
+  
   return (
-    <header className="header">
-      <div className="container">
+   <header className="header">
+  <div className="header-container">
 
-       <div className="header-left">
-        <div>
-            <img
-            src="/logo.svg"
-            alt="EMS Logo"
-            className='logo'
-          />
-        </div>
-         
-        </div>
+    <div className="header-left">
+      <img src="/logo.svg" alt="EMS Logo" className="logo" />
+    </div>
 
-       <div className="header-center">
-          <h4 className="nav-item">Dashboard</h4>
-          <h4 className="nav-item">All Events</h4>
-          <h4 className="nav-item">Manage</h4>
+    <div className="header-center">
+        <NavLink
+            to="/dashboard"
+            className={({ isActive }) =>
+              `nav-item ${isActive ? "active" : ""}`
+            }
+          >
+            Dashboard
+          </NavLink>
 
-          
+          <NavLink
+            to="/events"
+            className={({ isActive }) =>
+              `nav-item ${isActive ? "active" : ""}`
+            }
+          >
+            All Events
+          </NavLink>
 
-        
-        </div>
- <div className="header-right">
-          <p className='user-name'>Dan</p>
-        </div>
-       
-      </div>
-    </header>
+          <NavLink
+            to="/manage"
+            className={({ isActive }) =>
+              `nav-item ${isActive ? "active" : ""}`
+            }
+          >
+            Manage
+          </NavLink>
+    </div>
+
+    <div className="header-right">
+      <Avatar sx={{ bgcolor: deepOrange[500] }}>{user.AvatarLetter}</Avatar>
+      <p className="user-name">{user.name}</p>
+    </div>
+
+  </div>
+</header>
+
   )
 }
 
-export default Header
+export default Header;
